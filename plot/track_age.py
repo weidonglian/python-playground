f49ef90bf674c7a9b110e8f12d8fc23a10900e60
Weidong Lian <weidong.lian@gmail.com>
2020-02-06 16:00:17 +0100

    add track age calculation

---
+++ plot/track_age.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create the vectors X and Y
+exponent = -1.5
+const_exp = 0.77686983985157021
+max = 60 # 60 minutes
+x = np.array(range(0, 60))
+y = (1.0 - np.exp(exponent * x / 60 )) / const_exp
+
+# Create the plot
+plt.plot(x,y)
+
+# Show the plot
+plt.show()
